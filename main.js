288e9f392e4541797b14cf14234f3aedfc712612
Lucas Campagnoli <lucas.campagnoli@escola.pr.gov.br>
2025-06-17 09:34:03 -0300

    continuando

--- main.js
+++ main.js
@@ -23,14 +23,36 @@ function criarTabuleiro(){
         elemento.classList.add('snake');
         tabuleiro.appendChild(elemento);
     });
+    const elementoComida = document.createElement('div');
+    elementoComida.style.gridRowStart = comida.y;
+    elementoComida.style.gridColumnStart = comida.x;
+    elementoComida.classList.add('food');
+    tabuleiro.appendChild(elementoComida);
+}
     function geraComida(){
         let novaComida;
         while (!novaComida || posicaoOcupada(novaComida)){
             novaComida = {
                 x:Math.floor(Math.random()*18)+2,
-                y:Math.floor(Math.random()*18)+2
-            };
+                y:Math.floor(Math.random()*18)+2 
 
-        }
+        };
     }
+    return novaComida;
 }
+function posicaoOcupada(posicao){
+    return cobra.some(segmento => segmento.x === posicao.x && segmento.y===posicao.y);
+}
+function mudaDirecao(event){
+    const tecla = event.key;
+    if (tecla === "ArrowUp" && direcao !== 'BAIXO'){
+     direcao = 'CIMA';
+    } else if(tecla === 'ArrowDown' && direcao !== 'CIMA')
+    {
+        direcao = 'BAIXO';
+    }else if(tecla === 'ArrowLeft' && direcao !=='DIREITA'){
+        direcao = 'ESQUERDA';
+    } else if(tecla == 'ArrowRight' && direcao !=='ESQUERDA'){
+        direcao = 'DIREITA';
+    }
+} 
